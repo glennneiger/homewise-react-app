530d7a78a253739c9026e906f1acbe21818de01e
Visrut Sudhakar <visrut@live.unc.edu>
2018-06-12 14:44:18 -0400

    Revert "Revert to original"
    This reverts commit f0d9c2008107e1490aff8b9377a72806d1727816.

---
+++ HomeWiseFrontEnd copy/Router.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+ import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+import {
+  StackNavigator, TabNavigator
+} from 'react-navigation';
+
+import HomeScreen from './HomeScreen';
+import Login from './Login';
+import Registration from './Registration';
+import ForgotPassword from './ForgotPassword';
+import AllClients from './AllClients';
+import Steps from './Steps';
+
+export const Clients = StackNavigator(
+{
+  AllClients: {
+    screen: AllClients,
+  },
+  Steps:{
+    screen: Steps
+  }
+});
+
+
+export const Tabs = TabNavigator({
+  Clients: {
+    screen: Clients
+  }
+});
+
+export const HomeNav = StackNavigator(
+    {
+        HomeScreen: {
+            screen: HomeScreen,
+        },
+        Login: {
+            screen: Login,
+        },
+        Registration: {
+            screen: Registration,
+        },
+        ForgotPassword:{
+          screen: ForgotPassword
+        }
+    }
+);
